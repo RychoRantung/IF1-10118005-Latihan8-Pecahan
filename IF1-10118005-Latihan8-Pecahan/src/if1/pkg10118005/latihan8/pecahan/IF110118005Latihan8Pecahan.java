1fe040ff9a92fd61f307fdb85ea7b6385b46749d
Rycho Rantung <55700409+RychoRantung@users.noreply.github.com>
2019-09-26 13:34:51 +0700

    Add files via upload

---
+++ IF1-10118005-Latihan8-Pecahan/src/if1/pkg10118005/latihan8/pecahan/IF110118005Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118005.latihan8.pecahan;
+
+/**
+ *
+ * @author USER
+ * NAMA   : Rycho Rantung
+ * KELAS  : IF-1
+ * NIM    : 10118005
+ * Deskripsi Program : Program ini berisi program untuk menampilkan pecahan
+ */
+public class IF110118005Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
